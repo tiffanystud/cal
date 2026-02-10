618f6c44fe3a2092cba604560e4d7439245f660a
Tiffany <tiffanylarsson.stud@gmail.com>
2026-02-10 12:32:02 +0100

    CRUD Patch

--- neo-test/DBAccess.php
+++ neo-test/DBAccess.php
@@ -34,10 +34,13 @@ class DBAccess {
             $ids = array_column($db["users"], "id");
             $maxID = max($ids);
             $input["id"] = $maxID + 1;
+            
             array_push($db["users"], $input);
             DBIO::writeToDb($db);
             return $input;
+            
         } else if ($url === "/groups") {
+            
             if (!isset($input["name"])) {
                 return self::defaultResp("Attributes missing");
             }
@@ -45,10 +48,13 @@ class DBAccess {
             $ids = array_column($db["groups"], "id");
             $maxID = max($ids);
             $input["id"] = $maxID + 1;
+            
             array_push($db["groups"], $input);
             DBIO::writeToDb($db);
             return $input;
+            
         } else if ($url === "/users_groups") {
+            
             if (!isset($input["userID"]) or !isset($input["groupID"])) {
                 return self::defaultResp("Attributes missing");
             }
@@ -59,19 +65,95 @@ class DBAccess {
         }
     }
     public static function patchHandler($input) {
+        
         $url = $_SERVER["REQUEST_URI"];
         $db = DBIO::readDB();
+        
+        // PATCH: /users
         if($url === "/users") {
-            if(!isset($))
-
+            // Elias kör
 
             
-        } else if($url === "/groups") {
-            //
-        } else if($url == "/users_groups") {
+        // PATCH: /groups
+        } else if ($url === "/groups") {
+            
+            //Check if input contains ID and name
+            if (!isset($input["id"]) and !isset($input["name"]) ) {
+                return self::defaultResp("Attributes missing");
+            }
+            
+            $existingGroups = $db["groups"];
+            $newName = $input["name"];
+            
+            foreach($existingGroups as $currentGroup) {
+                // Update group name if ID found
+                if ($currentGroup["id"] == $input["id"]) {
+                    
+                    $currentGroup["name"] = $newName;
+                    return $currentGroup;
+                    
+                } else {
+                    
+                    return self::defaultResp(message: "Not found. Could not update.");
+                }
+                
+            }
+            // Hitta gruppen med id 
+            
+        // PATCH: /users_groups
+        } else if ($url == "/users_groups") {
 
+            if (!isset($input["me"]) || !isset($input["updates"])) {
+                return self::defaultResp("Missing me or updates");
+            }
+
+            //Req. attributen (två olika ass. arrs)
+            $me = $input["me"];
+            $updates = $input["updates"];
+        
+            // Me attributes
+            $meUserID = $me["userID"];
+            $meGroupID = $me["groupID"];
+            $meIsAdmin = false;
+            
+            // Target/update attributes
+            $targetUserID = $updates["userID"];
+            $updateAdminStatus = $updates["isAdmin"];
+
+            foreach ($db["users_groups"] as $currentUsersGroup) {
+
+                // Kolla om me finns i user_groups
+                if (
+                    $currentUsersGroup["userID"] == $meUserID &&
+                    $currentUsersGroup["groupID"] == $meGroupID
+                ) {
+                    // Kolla om den som skickar request (me) är admin
+                    if (!$currentUsersGroup["isAdmin"] == false) {
+                        return self::defaultResp("Unauthorized");
+                    }
+
+                // Hitta target som ska uppdateras
+                foreach($db["users_groups"] as $targetCurrentUserGroup) {
+                    if (
+                        // Kolla om me är admin i target group 
+                        $targetCurrentUserGroup["userID"] == $targetUserID && 
+                        $targetCurrentUserGroup["groupID"] == $meGroupID
+                        ) {
+                            $targetCurrentUserGroup["isAdmin"] = $updateAdminStatus;
+                            return $targetCurrentUserGroup;
+                        }
+                    }                    
+
+                    return self::defaultResp("Unauthorized");
+                }
+            }
+
+            return self::defaultResp("Not found");
+            
         } else {
+            
             return self::defaultResp("Attribute missing");
+            
         }
     }
 }
--- neo-test/HTTPHandler.php
+++ neo-test/HTTPHandler.php
@@ -16,27 +16,44 @@ class HTTPHandler {
             header("Access-Control-Allow-Origin: *");
         }
 
+        // Check requests content-type
         if (!isset($_SERVER["CONTENT_TYPE"])) {
+            
             http_response_code(400);
             return json_encode(["error" => "Content-Type not set, must be JSON"]);
+            
         } else if ($_SERVER["CONTENT_TYPE"] !== "application/json") {
+            
             http_response_code(400);
             return json_encode(["error" => "Unexpected Content-Type, must be JSON"]);
+            
         }
 
+        // Check requests methods
         if (!in_array($method, $allowedMethods)) {
+            
             http_response_code(405);
             return json_encode(["error" => "Method not allowed"]);
+            
         }
 
+        // Call right function in ABAccess 
         if ($method === "GET") {
+            
             return json_encode(DBAccess::getHandler());
+            
         } else if ($method === "POST") {
+            
             $input = file_get_contents("php://input");
             return json_encode(DBAccess::postHandler(json_decode($input, true)));
+            
         } else if($method === "PATCH") {
+            
+            // Save input
             $input = file_get_contents("php://input");
             
+            // Call patchHandler function wih the input data
+            return json_encode(DBAccess::patchHandler(json_decode($input, true)));
 
 
         } else if($method === "DELETE") {
