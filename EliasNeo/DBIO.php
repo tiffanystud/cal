79e73b2d70d879119100c564c7ab2c54828a52fb
Norrheden <eliasnorrhedenmau@gmail.com>
2026-02-12 14:37:36 +0100

    .

---
+++ EliasNeo/DBIO.php
@@ -0,0 +1,21 @@
+<?php
+
+    public static function readDb() {
+        $db = file_get_contents("db.json");
+        return json_decode($db, true);
+    }
+    public static function writeToDb($data) {
+        file_put_contents("db.json", json_encode($data, JSON_PRETTY_PRINT));
+        return true;
+    }
+
+    function DBIOHandler() {
+        
+    }
+
+
+
+
+
+
+?>
