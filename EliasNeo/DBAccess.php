fc71e5521b8248e0b7e89636fa516243883d8093
Neo <neogoldahlgren@gmail.com>
2026-02-12 13:10:09 +0100

    Added DBAccess and db files

---
+++ EliasNeo/DBAccess.php
@@ -0,0 +1,22 @@
+<?php
+require_once "DBIO.php";
+
+function methodHandler($method, $input = null) {
+    return $method($input);
+}
+
+function GET($input) {
+
+}
+
+function POST($input) {
+
+}
+
+function PATCH($input) {
+
+}
+
+function DELETE($input) {
+
+}
