657ae167d824c09ebd0b328338b15bedaabf4f4c
sjfalina <alinab91@hotmail.com>
2026-02-16 14:54:14 +0100

    fix action-function

--- alinav2/HTTPHandler.php
+++ alinav2/HTTPHandler.php
@@ -19,11 +19,8 @@ function action($method, $data) {
 
     if (!isset($handlers[$method])){
         $data = ["error" => "Method not allowed."];
-        return sendJSON($data, 405);
+        sendJSON($data, 405);
     }
-
-    $input = file_get_contents("php://input");
-    $data = $input ? json_decode($input, true) : null;
 
     $result = json_encode($handlers[$method]($data));
     sendJSON($result);
@@ -32,7 +29,6 @@ function action($method, $data) {
 
 function handleReq() {
     $method = $_SERVER["REQUEST_METHOD"];
-    $allowedMethods = ["GET", "POST", "PATCH", "DELETE"];
     header("Content-Type: application/json");
     if ($method === "OPTIONS") {
         header("Access-Control-Allow-Origin: *");
@@ -42,13 +38,21 @@ function handleReq() {
     } else {
         header("Access-Control-Allow-Origin: *");
     }
-    if (!isset($_SERVER["CONTENT_TYPE"])) {
-        $data = ["error" => "Content-Type not set, must be JSON"];
-        return sendJSON($data, 400);
-    } else if ($_SERVER["CONTENT_TYPE"] !== "application/json") {
-        $data = ["error" => "Unexpected Content-Type, must be JSON"];
-        return sendJSON($data, 400);
+
+    $bodyMethods = ["POST", "PATCH", "DELETE"];
+    if (in_array($method, $bodyMethods)){
+        if (!isset($_SERVER["CONTENT_TYPE"])) {
+            $data = ["error" => "Content-Type not set, must be JSON"];
+            sendJSON($data, 400);
+        } else if ($_SERVER["CONTENT_TYPE"] !== "application/json") {
+            $data = ["error" => "Unexpected Content-Type, must be JSON"];
+            sendJSON($data, 400);
+        }
     }
 
-    action($method);
+    $raw = file_get_contents("php://input");
+    $input = $raw ? json_decode($raw, true) : null;
+
+
+    action($method, $input);
 }
