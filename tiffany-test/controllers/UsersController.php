7565bbe65a1156f33f27f1cb90efddbf7379382d
philles85 <philster04@outlook.com>
2026-02-17 11:55:16 +0100

    Usercontroller coded

--- tiffany-test/controllers/UsersController.php
+++ tiffany-test/controllers/UsersController.php
@@ -1,18 +1,69 @@
 <?php
 class UsersController {
 
-    public static function handle($method, $input): void {
+    public static function handle($method, $input){
         if ($method === "GET") {
-            /* return UsersService::getAllUsers(); */
+            $db = UsersService::getAllUsers(); 
+
+            if(count($input) > 1){
+                foreach($db["User"] as $dbParameter => $dbObjectValue){
+                    foreach($requestInput as $requestParamter => $requestObjectValue){
+                        if(isset($dbObjectValue[$requestParamter]) && $dbObjectValue[$requestParamter] == $requestObjectValue){
+                            return $dbObjectValue;
+                        }
+                    }
+                }
+            } else {
+                foreach($db["User"] as $dbParameter => $dbObjectValue){
+                    foreach($requestInput as $requestParamter => $requestObjectValue){
+                        if(isset($dbObjectValue[$requestParamter]) && $dbObjectValue[$requestParamter] == $requestObjectValue){
+                            return $requestObjectValue;
+                        }
+                    }
+                }
+            }
         }
+        
         if ($method === "POST") {
-            /* return UsersService::getAllUsers(); */
+            $db = UsersService::getAllUsers(); 
+
+            if(count($input) > 1){
+                $countObjects = 0;
+                foreach($db["User"] as $dbParameter => $dbObjectValue){
+                    foreach($requestInput as $requestParamter => $requestObjectValue){
+                        if(isset($dbObjectValue[$requestParamter])){
+                            $countObjects++;
+                        }
+                    }
+                }
+                if($countObjects == count($input)){
+                    array_push($db[$arrayKey], $input);
+                    return $db;
+                }
+            }
         }
+
         if ($method === "PATCH") {
             /* return UsersService::getAllUsers(); */
         }
         if ($method === "DELETE") {
-            /* return UsersService::getAllUsers(); */
+            $db = UsersService::getAllUsers(); 
+
+            if(count($input) > 1){
+                $countObjects = 0;
+                foreach($db["User"] as $dbParameter => $dbObjectValue){
+                    foreach($requestInput as $requestParamter => $requestObjectValue){
+                        if(isset($dbObjectValue[$requestParamter]) && $dbObjectValue[$requestParamter] == $requestObjectValue){
+                            $countObjects++;
+                        }
+                    }
+
+                    if($countObjects == count($input)){
+                        $db[$arrayKey] = array_filter($db[$arrayKey], fn($tableObject) => $tableObject != $input);
+                        return $db;
+                    }
+                }
+            }
         }
 
     }
