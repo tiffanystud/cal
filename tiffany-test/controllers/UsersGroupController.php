5cccfab2cde06416583a6661f538e2ad06f20e77
sjfalina <alinab91@hotmail.com>
2026-02-17 21:35:41 +0100

    UsersGroupsController and Service
    Not done!

--- tiffany-test/controllers/UsersGroupController.php
+++ tiffany-test/controllers/UsersGroupController.php
@@ -1,15 +1,66 @@
 <?php
 
+require_once __DIR__ . "/../services/UsersGroupService.php";
+
 class UsersGroupController {
    public static function handler($method, $input): void {
         if ($method === "GET") {
-            UsersGroupService::getHandler();
+            try {
+                $id = $_GET["id"] ?? null;
+                $userId = $_GET["userId"] ?? null;
+                $groupId = $_GET["groupId"] ?? null;
+
+                if($id) {
+                    //Har ingen funktion ännu
+                } elseif ($userId){
+                    // Har ingen funktion men ev. getGroupOfUser?
+                } elseif ($groupId){
+                    // Denna funktion bör nog eg. ge en lista på användare.
+                    $result = UsersGroupService::getAllUsersInGroup($groupId);
+                    http_response_code(200);
+                    echo json_encode($result);
+                    return;
+                } else {
+                    // Har ingen funktion, vet inte, varför vill man ha alla users_groups?
+                }
+
+            } catch (Exception $exc){
+                http_response_code(400);
+                echo json_encode(["error" => $exc->getMessage()]);
+
+            }
+
         } else if ($method === "POST") {
-            UsersGroupService::postHandler();
+            try {
+                // addUserToGroup
+                $result = UsersGroupService::addUserToGroup($input);
+                http_response_code(200);
+                echo json_encode($result);
+                return;
+            } catch (Exception $exc){
+                http_response_code(400);
+                echo json_encode(["error" => $exc->getMessage()]);
+            }
         } else if($method === "PATCH") {
-            UsersGroupService::patchHandler();
+            try {
+                $result = UsersGroupService::makeUserGroupAdmin($input);
+                http_response_code(200);
+                echo json_encode($result);
+                return;
+            } catch (Exception $exc){
+                http_response_code(400);
+                echo json_encode(["error" => $exc->getMessage()]);
+            }
         } else if($method === "DELETE") {
-            UsersGroupService::deleteHandler();
+            try {
+                $result = UsersGroupService::removeUserFromGroup($input);
+                http_response_code(200);
+                echo json_encode($result);
+                return;
+            } catch (Exception $exc){
+                http_response_code(400);
+                echo json_encode(["error" => $exc->getMessage()]);
+            }
         }
    }
 
--- tiffany-test/services/UsersGroupService.php
+++ tiffany-test/services/UsersGroupService.php
@@ -1,16 +1,138 @@
 <?php
+
+require_once __DIR__ . "/../repositories/DBAccess.php";
+
 class UsersGroupsService {
-    public static function getAllUsers() {
-        /* return DBAcess::getUsers(); */
+
+    private DBAccess $users;
+    private DBAccess $groups;
+    private DBAccess $userGroups;
+
+    public function __construct()
+    {
+        $this->users = new DBAccess("users");
+        $this->groups = new DBAccess("groups");
+        $this->usersGroups = new DBAccess("user_groups");
     }
-    
-    public static function addUser($input) {
-    /*         
-        if (!isset($input["email"],$input["pwd"], $input["name"])) {
-            return;
+
+    public function addUserToGroup($input) {
+        if(!isset($input["userId"])){
+            throw new Exception("User ID missing.");
         }
-        return DBAcess::createUser($input) 
-    */
+        if(!isset($input["adminId"])){
+            throw new Exception("Admin ID missing.");
+        }
+        if(!isset($input["groupId"])){
+            throw new Exception("Group ID missing.");
+        }
+        $userId = $input["userId"];
+        $adminId = $input["adminId"];
+        $groupId = $input["groupId"];
+            
+        //Kolla om användaren finns!
+        $userExists = $this->users->findById($userId);
+        if (!$userExists){
+            throw new DomainException("User not found");
+        }
+        // Kolla om gruppen finns!  
+        $relations = $this->getAllUsersInGroup($groupId);
+        // Kolla om admin är admin!
+        $adminIsAdmin = array_any($relations, fn($x) => $x["userID"] == $adminId && $x["isAdmin"] == true);
+        if (!$adminIsAdmin) {
+            throw new DomainException("Only admin can add users to group.");
+        }
+        // Kolla om användaren finns i gruppen!
+        $userInGroup = array_any($relations, fn($x) => $x["userID"] == $userId);
+        if ($userInGroup) {
+            throw new DomainException("User already in group.");
+        }
+        $newId = newId($groupId);
+        // Här blir man då per default inte admin, men om man skapar en grupp måste man ju bli admin...
+        return $this->usersGroup->postData(["id" => $newId, "userID" => $userId, "groupID" => $groupId, "isAdmin" => false]);        
+
+        }
+    
+    public static function removeUserFromGroup($input) {
+        if(!isset($input["userId"])){
+            throw new Exception("User ID missing.");
+        }
+        if(!isset($input["adminId"])){
+            throw new Exception("Admin ID missing.");
+        }
+        if(!isset($input["groupId"])){
+            throw new Exception("Group ID missing.");
+        }
+        $userId = $input["userId"];
+        $adminId = $input["adminId"];
+        $groupId = $input["groupId"];
+
+        $relations = $this->getAllUsersInGroup($groupId);
+
+        $adminIsAdmin = array_any($relations, fn($x) => $x["userID"] == $adminId && $x["isAdmin"] == true);
+        if (!$adminIsAdmin) {
+            throw new DomainException("Only admin can add users to group.");
+        }
+        $userInGroup = array_find($relations, fn($x) => $x["userID"] == $userId);
+        if (!$userInGroup) {
+            throw new DomainException("User not in group.");
+        }
+        return $this->usersGroup->deleteData($userInGroup);    
+    }
+
+    //Denna funktion returnerar eg. inte alla användare i gruppen, men hela posten typ
+    // [id=>1, userID=>1, groupID=>1, isAdmin=>true]
+    public function getAllUsersInGroup($groupId) {
+        if (!$this->groups->findById($groupId)){
+            throw new DomainException("Group not found");
+        }
+
+        $relations = $this->usersGroups->getAll();
+        
+        return array_filter($relations, fn($x) => $x["groupID"] == $groupId);
+        
+    }
+
+    public function makeUserGroupAdmin($input){
+        if(!isset($input["userId"])){
+            throw new Exception("User ID missing.");
+        }
+        if(!isset($input["adminId"])){
+            throw new Exception("Admin ID missing.");
+        }
+        if(!isset($input["groupId"])){
+            throw new Exception("Group ID missing.");
+        }
+        $userId = $input["userId"];
+        $adminId = $input["adminId"];
+        $groupId = $input["groupId"];
+
+        $relations = $this->getAllUsersInGroup($groupId);
+
+        $adminIsAdmin = array_any($relations, fn($x) => $x["userID"] == $adminID && $x["isAdmin"] == true);
+        if (!$adminIsAdmin){
+            throw new Exception("Only admin can make user admin.");
+        }
+        $userInGroup = array_find($relations, fn($x) => $x["userID"] == $userId);
+
+        if(!$userInGroup){
+            throw new Exception("User is not in group.");
+        }
+
+        return $this->userGroups->patchData($userInGroup);
+
+        }
+
     }
     
-}
+    function newId($groupId){
+        $maxId = 0;
+        foreach ($this->usersGroups->getAll() as $uG){
+            if ($uG > $maxId){
+                $maxId = $uG;
+            }
+        }
+        return $maxId + 1;
+
+    }
+
+    
