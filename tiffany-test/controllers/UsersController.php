e290c06a4a84ef07e00f427a6b04cb549f655996
philles85 <philster04@outlook.com>
2026-02-17 12:34:50 +0100

    Fixed structure

--- tiffany-test/controllers/UsersController.php
+++ tiffany-test/controllers/UsersController.php
@@ -3,43 +3,29 @@ class UsersController {
 
     public static function handle($method, $input){
         if ($method === "GET") {
-            $db = UsersService::getAllUsers(); 
-
-            if(count($input) > 1){
-                foreach($db["User"] as $dbParameter => $dbObjectValue){
-                    foreach($requestInput as $requestParamter => $requestObjectValue){
-                        if(isset($dbObjectValue[$requestParamter]) && $dbObjectValue[$requestParamter] == $requestObjectValue){
-                            return $dbObjectValue;
-                        }
-                    }
-                }
-            } else {
-                foreach($db["User"] as $dbParameter => $dbObjectValue){
-                    foreach($requestInput as $requestParamter => $requestObjectValue){
-                        if(isset($dbObjectValue[$requestParamter]) && $dbObjectValue[$requestParamter] == $requestObjectValue){
-                            return $requestObjectValue;
-                        }
-                    }
-                }
+            if(!$input["userId"] > 1){
+                 // Anropa någon error funktion
+            } else if(empty(input["userName"])){
+                  // Anropa någon error funktion
+            } else if(!strlen($input["password"] > 5)){
+                 // Anropa någon error funktion
+            } else if(!str_contains($input["email"], "@")){
+                  // Anropa någon error funktion
             }
+            return UsersService::getAllUsers();
         }
         
         if ($method === "POST") {
-            $db = UsersService::getAllUsers(); 
-
-            if(count($input) > 1){
-                $countObjects = 0;
-                foreach($db["User"] as $dbParameter => $dbObjectValue){
-                    foreach($requestInput as $requestParamter => $requestObjectValue){
-                        if(isset($dbObjectValue[$requestParamter])){
-                            $countObjects++;
-                        }
-                    }
-                }
-                if($countObjects == count($input)){
-                    return $input;
-                }
+            if(!is_numeric($input["userId"])){
+                // Anropa någon error funktion
+            } else if(!ctype_upper($input["userName"][0])){
+                // Anropa någon error funktion
+            } else if(!str_contains($input["password"], "%") && !strlen($input["password"] > 5)){
+                  // Anropa någon error funktion
+            } else if(!str_contains($input["email"], "@")){
+                 // Anropa någon error funktion
             }
+            return UsersService::getAllUsers(); 
         }
 
         if ($method === "PATCH") {
--- tiffany-test/services/UserService.php
+++ tiffany-test/services/UserService.php
@@ -5,16 +5,50 @@
 <?php
 class UserService {
     public static function getAllUsers() {
-        /* return DBAcess::getUsers(); */
+        return DBAcess::getUsers();
+    }
+
+    public static function getUsers($input){
+
+        $db = UsersService::getAllUsers(); 
+
+            if(count($input) > 1){
+                foreach($db["User"] as $dbParameter => $dbObjectValue){
+                    foreach($requestInput as $requestParamter => $requestObjectValue){
+                        if(isset($dbObjectValue[$requestParamter]) && $dbObjectValue[$requestParamter] == $requestObjectValue){
+                            return $dbObjectValue;
+                        }
+                    }
+                }
+            } else {
+                foreach($db["User"] as $dbParameter => $dbObjectValue){
+                    foreach($requestInput as $requestParamter => $requestObjectValue){
+                        if(isset($dbObjectValue[$requestParamter]) && $dbObjectValue[$requestParamter] == $requestObjectValue){
+                            return $requestObjectValue;
+                        }
+                    }
+                }
+            }
+            
     }
     
     public static function createUser($input) {
-    /*         
-        if (!isset($input["email"],$input["pwd"], $input["name"])) {
-            return;
+        $db = UsersService::getAllUsers(); 
+
+        if(count($input) > 1){
+            $countObjects = 0;
+            foreach($db["User"] as $dbParameter => $dbObjectValue){
+                foreach($requestInput as $requestParamter => $requestObjectValue){
+                    if(isset($dbObjectValue[$requestParamter])){
+                        $countObjects++;
+                    }
+                }
+            }
+            if($countObjects == count($input)){
+                return $input;
+            }
         }
-        return DBAcess::createUser($input) 
-    */
+    
     }
     
 }
