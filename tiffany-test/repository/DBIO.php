5f9657cb53974f108026332576ecfb66169de7b5
sjfalina <alinab91@hotmail.com>
2026-02-18 14:55:13 +0100

    test path in DBIO

--- tiffany-test/repository/DBIO.php
+++ tiffany-test/repository/DBIO.php
@@ -1,13 +1,17 @@
 <?php
 
 class DBIO {
+    private static function dbPath(): string {
+    return __DIR__ . "/../db.json";
+}
+
     public static function readDb() {
-        $db = file_get_contents("db.json", true);
+        $db = file_get_contents(self::dbPath(), true);
         return json_decode($db, true);
     }
 
     public static function writeToDb($data) {
-        file_put_contents("db.json", json_encode($data, JSON_PRETTY_PRINT));
+        file_put_contents(self::dbPath(), json_encode($data, JSON_PRETTY_PRINT));
         return true;
     }
 
