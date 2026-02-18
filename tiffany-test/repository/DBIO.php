774348032b81d40b94a305af11a1c01d330dec5e
Tiffany <tiffanylarsson.stud@gmail.com>
2026-02-18 14:05:02 +0100

    Update DBIO.php

--- tiffany-test/repository/DBIO.php
+++ tiffany-test/repository/DBIO.php
@@ -8,7 +8,12 @@ class DBIO {
 
     public static function writeToDb($data) {
 <<<<<<< Updated upstream
+<<<<<<< Updated upstream
         file_put_contents("db.json", json_encode($data, JSON_PRETTY_PRINT), true);
+=======
+        $db = file_get_contents("db.json", true);
+        file_put_contents($db,json_encode($data, JSON_PRETTY_PRINT));
+>>>>>>> Stashed changes
 =======
         $db = file_get_contents("db.json", true);
         file_put_contents($db,json_encode($data, JSON_PRETTY_PRINT));
