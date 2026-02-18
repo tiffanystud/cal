4bf1b87149566fd21c4470f4147b2c7c0f03d7d7
Tiffany <tiffanylarsson.stud@gmail.com>
2026-02-18 11:51:41 +0100

    Add router, rename controller, remove HTTPHandler
    Add a new router.php that wires middleware and routes for users, groups and users_groups.

--- tiffany-test/controllers/GroupsController.php
+++ tiffany-test/controllers/GroupsController.php
@@ -3,7 +3,7 @@
 require_once __DIR__ . "/../services/GroupService.php";
 
 class GroupController {
-    public static function handler($method, $input): void {
+    public static function handle($method, $input): void {
 
         if ($method === "GET") {
             
---
+++ tiffany-test/controllers/router.php
@@ -0,0 +1,61 @@
+<?php
+
+// Middleware
+require_once __DIR__ . "/../middleware";
+
+// Controllera
+require_once "GroupController.php";
+require_once "UsersController.php";
+require_once "UsersGroupController.php";
+
+
+
+
+function Router($requestUrl){   
+
+    $urlPath = parse_url($requestUrl, PHP_URL_PATH);
+    $path = ltrim($urlPath, "/");
+    $method = $_SERVER["REQUEST_METHOD"];
+
+    switch ($path) {
+        case "users":
+            switch ($method) {
+               case "GET": 
+                    CorsMiddleware::handle();
+                    UsersController::handle(method: $method, input: []);
+                    break;
+                    
+                default:
+                    CorsMiddleware::handle();
+                    JsonMiddleware::handle();
+                    UsersController::handle(method: $method, input: []);
+                    break;
+            }
+        case "groups":
+            switch ($method) {
+               case "GET": 
+                    CorsMiddleware::handle();
+                    UsersController::handle(method: $method, input: []);
+                    break;
+                    
+                default:
+                    CorsMiddleware::handle();
+                    JsonMiddleware::handle();
+                    UsersController::handle(method: $method, input: []);
+                    break;
+            }
+            
+        case "users_groups":
+            switch ($method) {
+               case "GET": 
+                    CorsMiddleware::handle();
+                    UsersController::handle(method: $method, input: []);
+                    break;
+                default:
+                    CorsMiddleware::handle();
+                    JsonMiddleware::handle();
+                    UsersController::handle(method: $method, input: []);
+                    break;
+            }
+    }
+}
