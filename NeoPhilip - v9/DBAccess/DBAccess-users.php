c6cac9b490f4b5c5287a4f53d8845cadf12bc15c
philles85 <philster04@outlook.com>
2026-02-19 13:54:39 +0100

    DBAccess-users

---
+++ NeoPhilip - v9/DBAccess/DBAccess-users.php
@@ -0,0 +1,50 @@
+<?php
+
+require_once "./DBIO/DBIO-users.php";
+
+function usersHandler($method, $input, $url ){
+    return $method($input, $url);
+}
+
+function GET($input){
+    if(isset($input["id"])){
+        return getUserById($input);
+    } else {
+        return getAll();
+    }
+}
+
+function POST($input){
+    if(isset($input["name"]) && isset($input["email"]) && isset($input["password"])){
+        return postUser();
+    } else {
+        return ["error" => "Attributes missing", "code" => 400];
+    }
+}
+
+function PATCH($input){
+    if(isset($input["id"])){
+        $data = [];
+        $data["id"] = $input["id"];
+        if(isset($input["name"])){
+            $data["name"] = $input["name"];
+        } else if(isset($input["password"])){
+            $data["password"] = $input["password"];
+        } else if(isset($input["email"])){
+            $data["email"] = $input["email"];
+        } else {
+            return ["error" => "Attributes are missing", "code" => 400];
+        }
+        return patchUser($data);
+    } else {
+        return ["error" => "Id is missing", "code" => 400];
+    }
+}
+
+function DELETE($input){
+    if(isset($input["id"]) && isset($input["password"])){
+        return deleteUser($input);
+    } else {
+        return ["error" => "Attributes are missing", "code" => 400];
+    }
+}
---
+++ NeoPhilip - v9/DBIO/DBIO-users.php
@@ -0,0 +1,25 @@
+<?php
+
+function getAll(){
+
+}
+
+
+function getUserById($input){
+
+}
+
+
+function postUser($input){
+
+}
+
+
+function patchUser($input){
+
+}
+
+
+function deleteUser($input){
+    
+}
