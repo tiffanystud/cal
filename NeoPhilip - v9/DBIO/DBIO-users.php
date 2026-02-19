54884b2cf1adfa5299d8fad4b7b37eae62688ead
philles85 <philster04@outlook.com>
2026-02-19 15:38:16 +0100

    Started DBIO-users.php

--- NeoPhilip - v9/DBIO/DBIO-users.php
+++ NeoPhilip - v9/DBIO/DBIO-users.php
@@ -5,10 +5,32 @@ function getAll(){
 }
 
 function getUserById($input){
+    // $userTable = readDb()["users"];
+    $db = readDb();
+
+    $user = array_find($db["users"], fn($user) => $user["id"] == $input["id"]);
+
+    if($user){
+        return $user;
+    } else {
+        return ["error" => "User not found", "code" => 400];
+    }
 
 }
 
 function postUser($input){
+    $db = readTable();
+
+    $user = array_find($db, fn($user) => $user["id"] == $input["id"]);
+
+    if(!$user){
+        array_push($db["user"], $input);
+        writeToDb($db);
+        // Returns the created user
+        return $input;
+    } else {
+        return ["error" => "User already exists", "code" => 400];
+    }
 
 }
 
