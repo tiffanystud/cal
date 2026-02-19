fcebd1689a4825c936d34fce6183222223a7f96b
Neo <neogoldahlgren@gmail.com>
2026-02-19 14:43:50 +0100

    readWrite DBIO file nad small fixes

--- NeoPhilip - v9/DBIO/DBIO-readWrite.php
+++ NeoPhilip - v9/DBIO/DBIO-readWrite.php
@@ -1,7 +1,16 @@
 <?php
 
-function readDb() {
-    return json_decode(file_get_contents("../db/db.json", true));
+function readDb($backup = null) {
+    if (!$backup) {
+        return json_decode(file_get_contents("../db/db.json"), true);
+    } else {
+        return json_decode(file_get_contents("../db/db-backup.json"), true);
+    }
+}
+
+function resetDb() {
+    file_put_contents("../db/db.json", json_encode(readDb("backup"), JSON_PRETTY_PRINT));
+    return true;
 }
 
 function writeToDb($data) {
