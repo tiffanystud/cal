52bf07ea46d0e2b81030827867389047b0ab537e
Tiffany <tiffanylarsson.stud@gmail.com>
2026-02-20 14:54:07 +0100

    Scaffold AvailabilitiesControllers

---
+++ week8/controllers/AvailabilitiesController.php
@@ -0,0 +1,52 @@
+<?php
+
+require_once __DIR__ . "/../services/GroupsService.php";
+class AvailabilitiesController
+{
+
+    public static function handle($method, $input)
+    {
+
+        if ($method = "GET") {
+
+            try {
+
+            } catch (Exception $exc) {
+
+            }
+
+        }
+
+        if ($method = "GET") {
+
+            try {
+
+            } catch (Exception $exc) {
+
+            }
+            
+        }
+
+        if ($method = "GET") {
+
+            try {
+
+            } catch (Exception $exc) {
+
+            }
+            
+        }
+
+        if ($method = "GET") {
+
+            try {
+
+            } catch (Exception $exc) {
+
+            }
+            
+        }
+
+    }
+
+}
