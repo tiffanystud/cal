f7ccb95e63267183a5743acb2f8c26d5ae8054e2
philles85 <philster04@outlook.com>
2026-02-20 21:10:34 +0100

    Started with calenderController

--- week8/controllers/calendarsController.php
+++ week8/controllers/calendarsController.php
@@ -1,7 +1,59 @@
 <?php
 
-class calendarController{
+class calendarsController{
+    public static function calendarHandler($method, $input){
 
+        if($method == "GET"){
+            try{
+                if(empty($input)){
+                    return calendarsService::getAll();
+                } else {
+                    // Hämta en specifik calendar med id
+                    if(isset($input["id"])){
+                        return calendarsService::getById($input);
+    
+                    } else {
+                        return ["error" => "Value missing", "code" => 400];
+                    }
+                } 
+            } catch(Exception $error){
+
+            }       
+        }
+
+        if($method == "POST"){
+            try{
+                if(!isset($input["creatorId"]) || !isset($input["name"]) || !isset($input["type"])){
+                    return ["error" => "Attributes missing", "code" => 400];
+                } else {
+                    return calendarPost($input);
+                }
+            } catch(Exception $error){
+                // Kolla upp vad man kan returnera här och vad $error kommer bli sen
+                // Samma att servicen kommer returnera någon fel kod och då måste vi vet vilken kod det är här att skicka tillbaka
+            }
+
+        }
+
+        if($method == "PATCH"){
+            try{
+                if(!isset($input["id"])){
+                    
+                }
+
+            } catch(Exception $error){
+
+            }
+
+        }
+
+        if($method == "DELETE"){
+
+
+
+        }
+
+    }
 
 }
 
