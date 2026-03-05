484298ab507703cc7118cf2b6d9b365353708a9e
Norrheden <eliasnorrhedenmau@gmail.com>
2026-03-05 14:53:47 +0100

    Notify

--- CalApp/client/core/store.js
+++ CalApp/client/core/store.js
@@ -2,18 +2,20 @@ class Store {
     static allStates = [];
     constructor(initialState) {
         this.state = initialState;
-        Store.allStates.push(this.state)
+        Store.allStates.push(this.state);
+        this.listeners = []; 
     }
     getState() {
         return structuredClone(this.state);
     }
     setState(newState) {
         this.state = newState;
+        this.notify();
     }
-    subscribe() {
-
+    subscribe(listener) {
+        this.listeners.push(listener); 
     }
     notify() {
-
+        this.listeners.forEach(listener => listener());
     }
 }
