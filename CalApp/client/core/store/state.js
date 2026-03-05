09f4ed0cd3dbe99d712e59f51569a675add1517b
philles85 <philster04@outlook.com>
2026-03-05 15:59:46 +0100

    yes

--- CalApp/client/core/store/state.js
+++ CalApp/client/core/store/state.js
@@ -2,18 +2,24 @@ export const state = {
 
     isLoggedIn: { id: 1, username: "Elias", email: "Elias@mau.se" },
 
-    cals: [{ id: 1, creatorId: 2, name: "VIP", type: "public" }],
+    pages: {
+        currentPage: "",
+    },
 
-    event: [{ id: 1, date: "2026-03-05", type: "AW", name: "Inflyttningsfest", description: "Inflyttningsfest hos Elias", location: "Västra hamnen", calId: 1 }],
+    data: {
+        cals: [{ id: 1, creatorId: 2, name: "VIP", type: "public" }],
 
-    friends: [{ id: 1, userID1: 1, userID2: 2 }],
+        events: [{ id: 1, date: "2026-03-05", type: "AW", name: "Inflyttningsfest", description: "Inflyttningsfest hos Elias", location: "Västra hamnen", calId: 1 }],
 
-    privateMessages: [{ id: 1, senderId: 2, reciverId: 1, date: "2026-03-05", time: "15:15", content: "Plugga nu", hasChanged: false }],
+        friends: [{ id: 1, userID1: 1, userID2: 2 }],
 
-    calendarMessages: [{ id: 1, senderId: 2, calId: 1, date: "2026-03-05", time: "15:15", content: "Plugga nu", hasChanged: false }],
+        privateMessages: [{ id: 1, senderId: 2, reciverId: 1, date: "2026-03-05", time: "15:15", content: "Plugga nu", hasChanged: false }],
 
-    userPinnedCalendars: [{ id: 1, userId: 1, calId: 1 }],
+        calendarMessages: [{ id: 1, senderId: 2, calId: 1, date: "2026-03-05", time: "15:15", content: "Plugga nu", hasChanged: false }],
 
-    availabilites: [{ id: 1, userId: 1, date: "2026-03-05", isAvailable: true, calId: 1 }]
+        userPinnedCalendars: [{ id: 1, userId: 1, calId: 1 }],
+
+        availabilites: [{ id: 1, userId: 1, date: "2026-03-05", isAvailable: true, calId: 1 }]
+    }
 
 }
