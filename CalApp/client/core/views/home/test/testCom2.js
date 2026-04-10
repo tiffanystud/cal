2debf4ccf00414994164400a30205671ce0c836d
sjfalina <alinab91@hotmail.com>
2026-04-10 14:25:12 +0200

    not done template testCom2

--- CalApp/client/core/views/home/test/testCom2.js
+++ CalApp/client/core/views/home/test/testCom2.js
@@ -1,29 +1,59 @@
 /* Vid en specifik popup (ex. events?id=65afetd522) */
 import { PubSub } from "../../../store/pubsub";
 import { EVENTS } from "../../../store/events";
+import { store } from "../../../store/store.js";
 
 
 export class TestComp2 extends HTMLElement {
 
     constructor() {
-
         super();
         this.attachShadow({ mode: "open" });
-        this.subs()
 
+        this.loading = false;
+        this.data = null;
+
+        this.subs()
     }
 
     subs() {
 
-        PubSub.subscribe(EVENTS.VIEW.POPUP.SHOW.TEST1, () => {
+        PubSub.subscribe(EVENTS.VIEW.POPUP.SHOW.TEST1, (urlParams) => {
+            if (urlParams){
+                this.compService(data.subPath);
+            }
             this.openPopup();
+            
         })
 
         PubSub.subscribe(EVENTS.VIEW.POPUP.CLOSE.TEST1, () => {
             this.closePopup();
         })
 
+
     }
+
+    connectedCallback() {
+        // Komponentens egna "constructor" -> ?
+        this.render();
+
+    }
+
+    disconnectedCallback() {
+        // Ta bort globala lyssnare (om inaktiv komponent) -?
+        if (this.unsubscribe) this.unsubscribe();
+        this.closePopup();
+    }
+
+    async compService(subPath){
+        //kollar subpath, exvis ?id
+        // Om id inte finns i events, fetcha, annars 
+        if (subPath){
+              store.getState().test2
+        }
+
+    }
+
 
     getValue() {
         // Metod om komponent ska returnera något
@@ -43,15 +73,6 @@ export class TestComp2 extends HTMLElement {
         // koppla disc
     }
 
-    connectedCallback() {
-        // Komponentens egna "constructor" -> ?
-    }
-
-    disconnectedCallback() {
-        // Ta bort globala lyssnare (om inaktiv komponent) -?
-        if (this.unsubscribe) this.unsubscribe();
-        this.closePopup();
-    }
 
 
 }
