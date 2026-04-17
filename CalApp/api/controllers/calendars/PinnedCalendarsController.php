3718272c361bed3d6cbca623641b7ac6d5f50e29
Norrheden <eliasnorrhedenmau@gmail.com>
2026-04-17 11:01:28 +0200

    PinnedCal Controller

--- CalApp/api/controllers/calendars/PinnedCalendarsController.php
+++ CalApp/api/controllers/calendars/PinnedCalendarsController.php
@@ -3,6 +3,7 @@
 //
 
 require_once __DIR__ . "/../../services/PinnedCalendarsService.php";
+require_once __DIR__ . "/../sendJSON.php";
 
 class PinnedCalendarsController{
 
@@ -11,14 +12,22 @@ class PinnedCalendarsController{
         try {
 
             if($method == "GET"){
-            
-            } else if($method == "POST"){
-                
+                $data = PinnedCalendarsService::get();
+                sendJson([$data], 200);
 
-            } else if($method == "PATCH"){
-            
+            } else if($method == "POST"){
+                if(!isset($input["userId"]) || !isset($input["calId"])) {
+                    throw new Exception("Missing attributes");
+                }
+                $data = PinnedCalendarsService::post($input);
+                sendJson([$data],200);
+
             } else if($method == "DELETE"){
-            
+                if(!isset($input["userId"]) || !isset($input["calId"])) {
+                    throw new Exception("Missing attributes");
+                } 
+                $data = PinnedCalendarsService::delete($input);
+                sendJson([$data],200);
             }
 
         } catch(Exception $error) {
@@ -26,6 +35,26 @@ class PinnedCalendarsController{
         }
 
 
+
+    }
+    public static function errorHandler($error) {
+        $message = $error->getMessage(); 
+
+        //POST
+        if($message === "Missing attributes" ){
+            sendJson(["error" => "Missing attributes"], 400);
+        }
+        if($message === "Calendar already pinned" ){
+            sendJson(["error" => "Calendar already pinned"], 409);
+        }
+
+        //DELETE
+        if($message === "Missing attributes" ){
+            sendJson(["error" => "Missing attributes"], 400);
+        }
+        if($message === "Not found" ){
+            sendJson(["error" => "Not found"], 404);
+        }
 
     }
 
